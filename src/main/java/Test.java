c15604308a8287adb411884928e35166e19371e6
Arthur Hage <arthur.hage@C305-ads-036.fieb.org.br>
2015-11-18 15:57:08 -0300

    Endereco Test

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -1,4 +1,6 @@
 
+import com.ads.projetoIntegrador.dao.EnderecoDAO;
+import com.ads.projetoIntegrador.dto.EnderecoDTO;
 import com.ads.projetoIntegrador.test.PersonDAO;
 import com.ads.projetoIntegrador.test.PersonTestDTO;
 
@@ -20,5 +22,26 @@ public class Test {
         person.setName("TestName");
         dao.save(person);
         System.out.println(dao.find());
+        
+        EnderecoDAO end = new EnderecoDAO();
+        EnderecoDTO endereco = new EnderecoDTO();
+        endereco.setCidade("Salvador");
+        endereco.setEstado("Bahia");
+        endereco.setBairro("Imbui");
+        endereco.setRua("A");
+        endereco.setCep("123456");
+        endereco.setTelefone_primario("12345689");
+        end.save(endereco);
+        System.out.println(end.find());
+        
+        EnderecoDAO end2 = new EnderecoDAO();
+        EnderecoDTO endereco2 = new EnderecoDTO();
+        endereco2.setCidade("Salvador");
+        endereco2.setEstado("Bahia");
+        endereco2.setBairro("Imbui");
+        endereco2.setRua("A");
+        endereco2.setCep("123456");
+        endereco2.setTelefone_primario("12345689");
+        end2.save(endereco2);
     }
 }
