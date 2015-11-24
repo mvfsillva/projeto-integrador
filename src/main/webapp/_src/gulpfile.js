afccc42749e5780263a66eb84d4bd3fc7b1d3a21
Marcus Silva <marcusvinicius30798@gmail.com>
2015-11-24 20:05:00 -0300

    Moved gulpfile for folder _src

---
+++ src/main/webapp/_src/gulpfile.js
@@ -0,0 +1,46 @@
+'use strict';
+
+  var gulp        = require('gulp'),
+      stylus      = require('gulp-stylus'),
+      uglify      = require('gulp-uglify'),
+      concat      = require('gulp-concat'),
+      plumber     = require('gulp-plumber'),
+      jeet        = require('jeet'),
+      koutoSwiss  = require('kouto-swiss'),
+      rupture      = require('rupture'),
+      prefixer    = require('autoprefixer-stylus'),
+      imagemin    = require('gulp-imagemin'),
+      cp          = require('child_process');
+
+    gulp.task('stylus', function() {
+        gulp.src('assets/stylus/main.styl')
+        .pipe(plumber())
+        .pipe(stylus({
+            use:[jeet(), prefixer(), koutoSwiss(), rupture()],
+              compress: true
+          }))
+        .pipe(gulp.dest('../assets/css'))
+    });
+
+  gulp.task('js', function(){
+	   return gulp.src('/_src/js/**/*.js')
+		.pipe(plumber())
+		.pipe(concat('main.js'))
+		.pipe(uglify())
+		.pipe(gulp.dest('/assets/js/'))
+  });
+
+// gulp.task('imagemin', function() {
+// 	return gulp.src('./_src/images/**/*.{jpg,png,gif}')
+// 		.pipe(plumber())
+// 		.pipe(imagemin({ optimizationLevel: 3, progressive: true, interlaced: true }))
+// 		.pipe(gulp.dest('assets/img/'));
+// });
+
+gulp.task('watch', function () {
+	gulp.watch('assets/stylus/main.styl', ['stylus']);
+	gulp.watch('/_src/javascript/**/*.js', ['js']);
+	// gulp.watch('./_src/images/**/*.{jpg,png,gif}', ['imagemin']);
+});
+
+gulp.task('default', ['stylus', 'js', 'watch']);
