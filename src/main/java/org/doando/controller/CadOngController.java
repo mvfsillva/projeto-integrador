a6ce6959b3384b3656960dc525e3d6442dbc9fec
ahardmann <arthur.hardmann@hotmail.com>
2015-12-13 02:04:26 -0300

    remove import

--- src/main/java/org/doando/controller/CadOngController.java
+++ src/main/java/org/doando/controller/CadOngController.java
@@ -6,7 +6,6 @@ import java.util.HashSet;
 import java.util.List;
 
 import javax.faces.bean.ManagedBean;
-import javax.faces.bean.SessionScoped;
 import javax.faces.bean.ViewScoped;
 
 import org.doando.appService.OngApplicationService;
